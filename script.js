ecdeabf0b3638b8fd0cc26230aa689aaea4e5d97
kpeca <40670357+kpeca@users.noreply.github.com>
2021-02-02 15:30:47 +0100

    dodavanje statistike

---
+++ script.js
@@ -0,0 +1,18 @@
+function toggle() {
+  var blur = document.getElementById("blur");
+  blur.classList.toggle("active");
+  var popup = document.getElementById("popup");
+  popup.classList.toggle("active");
+}
+function statistika() {
+  var blur = document.getElementById("blur");
+  blur.classList.toggle("active");
+  var statistika = document.getElementById("statistika");
+  statistika.classList.toggle("active");
+}
+function zabava() {
+  var blur = document.getElementById("blur");
+  blur.classList.toggle("active");
+  var zabava = document.getElementById("zabava");
+  zabava.classList.toggle("active");
+}
